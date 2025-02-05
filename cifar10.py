b48813f9de1787b124af3ad89c1c4e042cd7becd
kf5131 <kf5131@icloud.com>
2025-02-05 13:26:11 +0100

    add plot1

--- cifar10.py
+++ cifar10.py
@@ -12,3 +12,16 @@ x_test = x_test / 255.0
 # Define class names for better understanding
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
+
+# Display first 25 images from the training set
+plt.figure(figsize=(10, 5))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_train[i])
+    plt.xlabel(class_names[y_train[i][0]])
+plt.show()
+
+
