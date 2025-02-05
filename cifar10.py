45de354362c126d209e4ac1a3c3cf2e14e2b5155
kf5131 <kf5131@icloud.com>
2025-02-05 13:23:53 +0100

    add loading and preprocessing

--- cifar10.py
+++ cifar10.py
@@ -1,3 +1,14 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Load CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Define class names for better understanding
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
